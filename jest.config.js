52bdb3082a4ca39511f41dff0cb30ba9e9cbeba8
Melissa Freiser <17129292+mofoshow@users.noreply.github.com>
2022-07-07 13:38:41 +0200

    Setup next js project with styled components, jest, prettier and eslint

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+export default createJestConfig(customJestConfig)
